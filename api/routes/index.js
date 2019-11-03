f13833c4eeb18de426b57c146b67bc10bd5d78ac
Wayne-Lutz <waynelutz1@gmail.com>
2019-11-03 13:10:00 -0500

    Started using the Raspistill to capture images and Jimp to compare.

--- api/routes/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const alarmRoutes = require('./alarm-routes');
-// const utilRoutes = require('./utilRoutes');
-
-module.exports = function(app) {
-	alarmRoutes(app);
-	// utilRoutes(app);
-};
