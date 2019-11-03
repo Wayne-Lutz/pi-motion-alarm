a8b151cbb00966f687be511bfd88f67e99e019e5
Wayne-Lutz <waynelutz1@gmail.com>
2019-11-03 13:51:36 -0500

    Implemented the MotionAlarm._detectMotion() method.

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const motionAlarm = require('./lib/motion-alarm');
 
 const app = express();
 
-app.use(logger('dev'));
+// app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
